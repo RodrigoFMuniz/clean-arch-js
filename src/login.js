854c6786179e48cb7dbd6f1580ba3883455d970f
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-03-10 14:42:21 -0300

    Add husky new config feature

---
+++ src/login.js
@@ -0,0 +1,12 @@
+// const express = require('express')
+// const router = express.Router()
+// const mongoose = require('mongoose')
+// const AccountModel = mongoose.model('Account')
+
+// module.exports = ()=>{
+//     router.post('/signup',)
+// }
+
+// class SignUpRouter{
+
+// }
